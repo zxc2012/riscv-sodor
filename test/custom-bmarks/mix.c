18a5bdef4a30865e699e5b97c995d627520ffce5
zxc2012 <786698925@qq.com>
2022-03-28 13:45:05 +0800

    first commit

---
+++ test/custom-bmarks/mix.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    for(int i= -__INT_MAX__-1;i<__INT_MAX__;i++);
+    return 0;
+}
